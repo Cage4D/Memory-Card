1601b9c63267323b7c20499babd27071187a9be2
Br0cod3 <thespammingbott@gmail.com>
2025-12-17 21:42:04 +0100

    add: header custom font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 
 function Header({ score, bestScore }) {
     return (
-        <nav className="sticky top-0 flex flex-col items-center py-5 mb-10 gap-2 bg-blue-600 text-white">
-            <h2 className="">Rick and Morty Memory Game</h2>
+        <nav className="sticky top-0 flex flex-col items-center py-5 mb-10 gap-2 bg-blue-600 text-white font-russo">
+            <h2 className="text-xl">Rick and Morty Memory Game</h2>
             <div className="flex justify-center gap-2 whitespace-nowrap">
                 <p className="flex-1">Score: {score}</p>
                 <p className="flex-1">Best Score: {bestScore}</p>
             </div>
-            <p>Get points by clicking on an image but don't click on any more than once!</p>
+            <p className="italic">Get points by clicking on an image but don't click on any more than once!</p>
         </nav>
     )
 }
