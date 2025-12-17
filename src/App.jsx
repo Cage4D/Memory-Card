0e03f7150a2deee02314adef2dd7b6e0f9d7c1a4
Br0cod3 <thespammingbott@gmail.com>
2025-12-17 15:39:27 +0100

    feat: import component files & implement application and data fetching logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
+import React from "react"
 import './App.css'
+import Card from "./components/Card"
+import Header from "./components/Header";
 
 function App() {
+    const [score, setScore] = React.useState(0);
+    const [bestScore, setBestScore] = React.useState(0)
+    const [cardData, setCardData] = React.useState([])
 
+    function updateScore() {
+        setScore(score + 1)
+    }
+
+    function resetScore() {
+        setScore(0)
+    }
+
+    function updateBestScore() {
+        setBestScore(score)
+    }
+
+    React.useEffect(() => {
+        async function getData() {
+            try {
+               const response = await fetch("https://rickandmortyapi.com/api/character")
+               if (!response.ok) throw new Error(`Response status: ${response.status}`)
+                const result = await response.json()
+                const data = result.results.slice(0, 16)
+                setCardData(data)
+           } catch(e) {
+               console.error(e.message)
+           }
+        }
+
+        getData()
+    }, [])
+
+    return (
+        <div>
+            <div>
+                <Header score={score} bestScore={bestScore}/>
+            </div>
+            <div className=" h-full w-full flex gap-7 flex-wrap justify-center">
+                <Card data={cardData}/>
+            </div>
+        </div>
+    )
 }
 
 export default App
